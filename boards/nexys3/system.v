d9b38f4316c83ad0bcd8672f2b2c4a4d6f442b56
Andreas Galauner <andreas@galauner.de>
2012-07-14 03:23:58 +0200

    vga sync generator rewritten and moved into a submodule, simple 2-line ram added which gets drawn via vga

--- boards/nexys3/system.v
+++ boards/nexys3/system.v
@@ -14,83 +14,50 @@ module system (
 wire rst_n;
 assign rst_n = ~btns;
 
-//50MHz pixelclock
-reg vga_clk;
-always @(posedge clk)
-    vga_clk <= ~rst_n ? 0 : ~vga_clk;
+wire        vga_clk;
+wire        vga_active;
+wire [10:0] CounterX;
+wire [ 9:0] CounterY;
 
-/*  800x600@72Hz
+vga_sync sync (
+    .clk(clk),
+    .rst_n(rst_n),
 
-        General timing
-Screen refresh rate     72 Hz
-Vertical refresh        48.076923076923 kHz
-Pixel freq.             50.0 MHz
+    .pixelclock(vga_clk),
+    .hsync(Hsync),
+    .vsync(Vsync),
+    .displayactive(vga_active),
+    .counterX(CounterX),
+    .counterY(CounterY)
+);
 
-        Horizontal timing (line)
-Scanline part   Pixels  Time [µs]
-Visible area    800     16
-Front porch     56      1.12
-Sync pulse      120     2.4
-Back porch      64      1.28
-Whole line      1040    20.8
+wire [10:0] xx = CounterX;
+wire        comp_read = CounterY[1];
+wire [ 8:0] comp_scanout = xx[9:1];
+wire [ 9:0] ram_read_address = {!comp_read, comp_scanout};
 
-        Vertical timing (frame)
-Frame part      Lines   Time [ms]
-Visible area    600     12.48
-Front porch     37      0.7696
-Sync pulse      6       0.1248
-Back porch      23      0.4784
-Whole frame     666     13.8528*/
-`define HSTART 0
+wire [7:0] comp_out;
+bram_tdp #(
+    .DATA_WIDTH(8),
+    .ADDR_WIDTH(10),
+    .MEM_FILE_NAME("../../data/data.ram")
+) line_ram (
+    .a_clk(0),
+    .a_wr(0),
+    .a_addr(10'b0),
+    .a_din(8'h00),
+    .a_dout(),
 
-reg [10:0] CounterX;
-reg [9:0] CounterY;
-reg vga_HS, vga_VS, vga_active;
+    .b_clk(vga_clk),
+    .b_wr(0),
+    .b_addr(ram_read_address),
+    .b_din(8'h00),
+    .b_dout(comp_out)
+);
 
-wire CounterXmaxed = (CounterX==1040);
-wire lastline = (CounterY == 665);
-wire [9:0] _CounterY = lastline ? 0 : (CounterY + 1);
-
-always @(posedge vga_clk or negedge rst_n)
-    if(~rst_n)
-        CounterX <= 0;
-    else
-        if(CounterXmaxed)
-            CounterX <= 0;
-        else
-            CounterX <= CounterX + 1;
-
-always @(posedge vga_clk or negedge rst_n)
-    if(~rst_n)
-        CounterY <= 0;
-    else
-        if (CounterXmaxed)
-            CounterY <= _CounterY;
-
-always @(posedge vga_clk) begin
-    vga_HS <= ((800 + 61) <= CounterX) & (CounterX < (800 + 61 + 120));
-    vga_VS <= (35 <= CounterY) & (CounterY < (35 + 6));
-    vga_active <= ((`HSTART + 1) <= CounterX) & (CounterX < (`HSTART + 1 + 800)) & ((35 + 6 + 21) < CounterY) & (CounterY <= (35 + 6 + 21 + 600));
-end
-
-assign Hsync = vga_HS;
-assign Vsync = vga_VS;
-
-reg [8:0] colors;
-always @(posedge vga_clk or negedge rst_n) begin
-    if(~rst_n) begin
-        colors <= 8'h00;
-    end else begin
-        if(vga_active) begin
-            colors <= colors + 1;
-        end
-    end
-end
-
-assign vgaRed = (vga_active) ? colors[7:5] : 3'b0;
-assign vgaGreen = (vga_active) ? colors[4:2] : 3'b0;
-assign vgaBlue = (vga_active) ? colors[1:0] : 2'b0;
-
+assign vgaRed = vga_active ? comp_out[7:5] : 3'b0;
+assign vgaGreen = vga_active ? comp_out[4:2] : 3'b0;
+assign vgaBlue = vga_active ? comp_out[1:0] : 2'b0;
 
 //Fancy LED blinky stuff
 reg [25:0] counter;
---
+++ rtl/vga_sync.v
@@ -0,0 +1,125 @@
+module vga_sync (
+   input         clk,
+   input         rst_n,
+
+   output        pixelclock,
+   output        hsync,
+   output        vsync,
+   output        displayactive,
+   output [10:0] counterX,
+   output [ 9:0] counterY
+);
+
+//generate 50MHz pixelclock
+reg vga_clk;
+always @(posedge clk)
+    vga_clk <= ~rst_n ? 0 : ~vga_clk;
+
+assign pixelclock = vga_clk;
+
+/*  800x600@72Hz
+
+        General timing
+Screen refresh rate     72 Hz
+Vertical refresh        48.076923076923 kHz
+Pixel freq.             50.0 MHz
+
+        Horizontal timing (line)
+Scanline part   Pixels  Time [µs]
+Visible area    800     16
+Front porch     56      1.12
+Sync pulse      120     2.4
+Back porch      64      1.28
+Whole line      1040    20.8
+
+        Vertical timing (frame)
+Frame part      Lines   Time [ms]
+Visible area    600     12.48
+Front porch     37      0.7696
+Sync pulse      6       0.1248
+Back porch      23      0.4784
+Whole frame     666     13.8528*/
+
+`define H_DISPLAY       800
+`define H_BACKPORCH     63
+`define H_SYNC          120
+`define H_FRONTPORCH    56
+`define H_TOTALPERIOD   `H_DISPLAY + `H_BACKPORCH + `H_SYNC + `H_FRONTPORCH
+
+`define V_DISPLAY       600
+`define V_BACKPORCH     23
+`define V_SYNC          6
+`define V_FRONTPORCH    37
+`define V_TOTALPERIOD   `V_DISPLAY + `V_BACKPORCH + `V_SYNC + `V_FRONTPORCH
+
+reg [10:0]  CounterX;
+reg [ 9:0]  CounterY;
+reg         hsyncpulse;
+reg         vga_HS, vga_VS, vga_active;
+
+//horizontal counter
+always @(posedge vga_clk or negedge rst_n) begin
+    if(~rst_n) begin
+        CounterX <= 0;
+    end else begin
+        if(CounterX == `H_TOTALPERIOD)
+            CounterX <= 0;
+        else
+            CounterX <= CounterX + 1;
+    end
+end
+assign counterX = CounterX;
+
+//vertical counter
+always @(posedge vga_clk or negedge rst_n) begin
+    if(~rst_n) begin
+        CounterY <= 0;
+    end else begin
+        if(CounterY == `V_TOTALPERIOD)
+            CounterY <= 0;
+        else if(CounterX == `H_TOTALPERIOD)
+            CounterY <= CounterY + 1;
+    end
+end
+assign counterY = CounterY;
+
+//hsync generation
+always @(posedge vga_clk or negedge rst_n) begin
+    if(~rst_n) begin
+        vga_HS <= 0;
+    end else begin
+        if(CounterX == `H_DISPLAY + `H_BACKPORCH)
+            vga_HS <= 0;
+        else if(CounterX == `H_TOTALPERIOD - `H_FRONTPORCH)
+            vga_HS <= 1;
+    end
+end
+assign hsync = ~vga_HS;
+
+//vsync generation
+always @(posedge vga_clk or negedge rst_n) begin
+    if(~rst_n) begin
+        vga_VS <= 0;
+    end else begin
+        if(CounterY == `V_DISPLAY + `V_BACKPORCH)
+            vga_VS <= 0;
+        else if(CounterY == `V_TOTALPERIOD - `V_FRONTPORCH)
+            vga_VS <= 1;
+    end
+end
+assign vsync = ~vga_VS;
+
+//display active signal generation
+always @(posedge vga_clk or negedge rst_n) begin
+    if(~rst_n) begin
+        vga_active <= 0;
+    end else begin
+        if(!(CounterX < `H_DISPLAY && CounterY < `V_DISPLAY))
+            vga_active <= 0;
+        else
+            vga_active <= 1;
+    end
+end
+assign displayactive = vga_active;
+
+endmodule
