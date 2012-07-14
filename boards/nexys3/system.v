4f2ddad1ef8920011a27e41cc72e3efd797790bd
Andreas Galauner <andreas@galauner.de>
2012-07-14 06:29:58 +0200

    Line-RAM is filled and scanned

--- boards/nexys3/system.v
+++ boards/nexys3/system.v
@@ -31,10 +31,79 @@ vga_sync sync (
     .counterY(CounterY)
 );
 
-wire [10:0] xx = CounterX;
-wire        comp_read = CounterY[1];
-wire [ 8:0] comp_scanout = xx[9:1];
-wire [ 9:0] ram_read_address = {!comp_read, comp_scanout};
+wire [ 8:0] workX = CounterX[9:1];
+wire [ 9:0] workY = CounterY;
+wire        workBank = CounterY[0];
+wire        work_en = CounterX < 11'h400 && CounterY < 11'h400;
+wire [ 9:0] work_write_address = {workBank, workX};
+
+//4K RAM which holds pointer into chardata_ram for 64x64 grid of characters to be displayed
+bram_tdp #(
+    .DATA_WIDTH(8),
+    .ADDR_WIDTH(12),
+    .MEM_FILE_NAME("../../data/chars.ram")
+) char_ram (
+    .a_clk(0),
+    .a_ena(1),
+    .a_wr(0),
+    .a_addr(12'b0),
+    .a_din(8'h00),
+    .a_dout(),
+
+    .b_clk(vga_clk),
+    .b_ena(1),
+    .b_wr(0),
+    .b_addr(12'b0),
+    .b_din(8'h00),
+    .b_dout()
+);
+
+//4K RAM with pixeldata of glyps, 1 byte holds 4 pixel (index into char color table)
+bram_tdp #(
+    .DATA_WIDTH(8),
+    .ADDR_WIDTH(12),
+    .MEM_FILE_NAME("../../data/chardata.ram")
+) chardata_ram (
+    .a_clk(0),
+    .a_ena(1),
+    .a_wr(0),
+    .a_addr(12'b0),
+    .a_din(8'h00),
+    .a_dout(),
+
+    .b_clk(vga_clk),
+    .b_ena(1),
+    .b_wr(0),
+    .b_addr(12'b0),
+    .b_din(8'h00),
+    .b_dout()
+);
+
+//2K RAM which holds 4 colors for each possible character (16bit color * 4 colors * 256 chars)
+//only 8 bit for colors used currently, bit 9 = transparency
+bram_tdp #(
+    .DATA_WIDTH(8),
+    .ADDR_WIDTH(11),
+    .MEM_FILE_NAME("../../data/charpal.ram")
+) charpal_ram (
+    .a_clk(0),
+    .a_ena(1),
+    .a_wr(0),
+    .a_addr(11'b0),
+    .a_din(8'h00),
+    .a_dout(),
+
+    .b_clk(vga_clk),
+    .b_ena(1),
+    .b_wr(0),
+    .b_addr(11'b0),
+    .b_din(8'h00),
+    .b_dout()
+);
+
+wire        scanBank = CounterY[0];
+wire [ 8:0] scanX = CounterX[9:1];
+wire [ 9:0] scan_read_address = {~scanBank, scanX};
 
 wire [7:0] comp_out;
 bram_tdp #(
@@ -42,15 +111,17 @@ bram_tdp #(
     .ADDR_WIDTH(10),
     .MEM_FILE_NAME("../../data/data.ram")
 ) line_ram (
-    .a_clk(0),
-    .a_wr(0),
-    .a_addr(10'b0),
-    .a_din(8'h00),
+    .a_clk(vga_clk),
+    .a_ena(work_en),
+    .a_wr(1),
+    .a_addr(work_write_address),
+    .a_din(work_write_address[7:0]),                  //just for testing
     .a_dout(),
 
     .b_clk(vga_clk),
+    .b_ena(1),
     .b_wr(0),
-    .b_addr(ram_read_address),
+    .b_addr(scan_read_address),
     .b_din(8'h00),
     .b_dout(comp_out)
 );
--- rtl/bram_tdp.v
+++ rtl/bram_tdp.v
@@ -6,18 +6,20 @@ module bram_tdp #(
     parameter MEM_FILE_NAME = "none"
 ) (
     // Port A
-    input   wire                a_clk,
-    input   wire                a_wr,
-    input   wire    [ADDR_WIDTH-1:0]  a_addr,
-    input   wire    [DATA_WIDTH-1:0]  a_din,
-    output  reg     [DATA_WIDTH-1:0]  a_dout,
+    input   wire                  a_clk,
+    input   wire                  a_ena,
+    input   wire                  a_wr,
+    input   wire [ADDR_WIDTH-1:0] a_addr,
+    input   wire [DATA_WIDTH-1:0] a_din,
+    output  reg  [DATA_WIDTH-1:0] a_dout,
 
     // Port B
-    input   wire                b_clk,
-    input   wire                b_wr,
-    input   wire    [ADDR_WIDTH-1:0]  b_addr,
-    input   wire    [DATA_WIDTH-1:0]  b_din,
-    output  reg     [DATA_WIDTH-1:0]  b_dout
+    input   wire                  b_clk,
+    input   wire                  b_ena,
+    input   wire                  b_wr,
+    input   wire [ADDR_WIDTH-1:0] b_addr,
+    input   wire [DATA_WIDTH-1:0] b_din,
+    output  reg  [DATA_WIDTH-1:0] b_dout
 );
 
 // Shared memory
@@ -25,19 +27,23 @@ reg [DATA_WIDTH-1:0] mem [(2**ADDR_WIDTH)-1:0];
 
 // Port A
 always @(posedge a_clk) begin
-    a_dout      <= mem[a_addr];
-    if(a_wr) begin
-        a_dout      <= a_din;
-        mem[a_addr] <= a_din;
+    if(a_ena) begin
+        a_dout      <= mem[a_addr];
+        if(a_wr) begin
+            a_dout      <= a_din;
+            mem[a_addr] <= a_din;
+        end
     end
 end
 
 // Port B
 always @(posedge b_clk) begin
-    b_dout      <= mem[b_addr];
-    if(b_wr) begin
-        b_dout      <= b_din;
-        mem[b_addr] <= b_din;
+    if(b_ena) begin
+        b_dout      <= mem[b_addr];
+        if(b_wr) begin
+            b_dout      <= b_din;
+            mem[b_addr] <= b_din;
+        end
     end
 end
 
